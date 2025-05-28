8db6a82bdd3d1fd08534c9a4cc2baf9f5356e669
Faith Ugwo <faithtega14@gmail.com>
2025-05-28 09:41:06 +0000

    Add Next.js project setup

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,10 @@
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
+// This code defines a simple React functional component for an "About" page.
